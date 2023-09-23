2b24c19855284f2081cf2910c1c161e6d927e589
electrone <Carbajalluis@rocketmail.com>
2023-09-23 01:54:15 -0400

    add FE components

---
+++ data.js
@@ -0,0 +1,60 @@
+export const quizData = {
+  author: "0x021B278a90fC4764e97d00c73966e36b",
+  title: "Master the Fundamentals of React",
+  max_Points: "100",
+  description:
+    "Master the Fundamentals of React is an essential course designed to provide a solid foundation in React, the popular JavaScript library for building user interfaces. This course covers key concepts such as component development, JSX syntax, state management, and routing, empowering learners to create interactive and dynamic web applications.",
+  questions: [
+    {
+      question: "What is React JS?",
+      options: [
+        { a: "It is a JavaScript library for building user interfaces" },
+        { b: "It is a type of coffee" },
+        { c: "It is a dessert" },
+        { d: "It is a sports team" },
+      ],
+      correct: "a",
+    },
+
+    {
+      question: "Which of the following concepts is central to React?",
+      options: [
+        { a: "Component-based architecture" },
+        { b: "Database management" },
+        { c: "Machine learning" },
+        { d: "Networking" },
+      ],
+      correct: "a",
+    },
+    {
+      question: "What does JSX stand for in React?",
+      options: [
+        { a: "JavaScript XML" },
+        { b: "JSON Extensible" },
+        { c: "Java Server Extension" },
+        { d: "JavaScript XML Syntax" },
+      ],
+      correct: "a",
+    },
+    {
+      question: "What function is used to render a React component to the DOM?",
+      options: [
+        { a: "ReactDOM.render()" },
+        { b: "React.renderComponent()" },
+        { c: "renderToDOM()" },
+        { d: "renderComponentToDOM()" },
+      ],
+      correct: "a",
+    },
+    {
+      question: "Which lifecycle method is called after a component is removed from the DOM?",
+      options: [
+        { a: "componentWillUnmount" },
+        { b: "componentDidMount" },
+        { c: "componentWillMount" },
+        { d: "componentDidUpdate" },
+      ],
+      correct: "a",
+    },
+  ],
+};
